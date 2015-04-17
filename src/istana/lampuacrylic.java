79ab2a9b64eff7935affe5f3f4f60cb92bc67017
SBC4 <SBC4@SBC10>
2015-04-17 10:25:45 +0700

    buat gantungan acrylic

---
+++ src/istana/lampuacrylic.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package istana;
+
+/**
+ *
+ * @author SBC4
+ */
+public class lampuacrylic {
+    
+}
